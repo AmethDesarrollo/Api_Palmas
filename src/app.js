938424ab4f43f6496b503286d9a72c70c1514991
joestark96 <alfonsomtz.96@gmail.com>
2022-05-23 14:12:21 -0600

    cors config

--- src/app.js
+++ src/app.js
@@ -18,10 +18,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-app.use(cors({
-    origin:"*",
-    credentials:true
-}));
+app.use(cors());
 
 app.use(function (err, req, res, next) {
   res.header('Access-Control-Allow-Origin', "*");
